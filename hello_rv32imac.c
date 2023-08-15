b706de1efbd830edf61cbb1f33895f7b2a00a224
easonLiang <liangyx25@163.com>
2023-08-15 09:20:28 +0800

    sdk_easonLiang

---
+++ hello_rv32imac.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    printf("Language : C \n");
+    printf("Author : EasonLiang \n");
+    printf("Env Type : Regressive Integration \n");
+    printf("Build Platform : ArchLinux @ 20230814\n");
+    printf("Running Platform : Emulated RISC-V \n");
+}
